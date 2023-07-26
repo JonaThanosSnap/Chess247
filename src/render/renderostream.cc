e5d611a091e901147d50e76adfdc66916a8bc09e
Matthews Ma <matthewsma21@gmail.com>
2023-07-26 01:21:49 -0400

    Create render method for ostream with placeholder methods
    - Board::getPiece(Coordinate)
    - Piece::getChar()
    - Coordinate::isWhite()

--- src/render/renderostream.cc
+++ src/render/renderostream.cc
@@ -1,1 +1,40 @@
 #include "renderostream.h"
+#include "board.h"
+#include "piece.h"
+#include "coordinate.h"
+#include <iostream>
+
+class Piece;
+
+RenderOStream::RenderOStream(Board* board, std::ostream& out): 
+    ChessRender{board}, out{out} {};
+
+void RenderOStream::render() const {
+    for (int i=0; i < 8; i++) {
+        // rank
+        out << 8-i << " ";
+
+        for (int j=0; j < 8; j++) {
+            Coordinate c{i, j};
+            Piece* piece = board->getPiece(c);
+            
+            if (piece != nullptr) {
+                // there is a piece on this square
+                out << piece->getChar();
+            } else {
+                // this square is empty
+                out << c.isWhite() ? ' ' : '_';
+            }
+        }
+        out << std::endl;
+    }
+    out << std::endl;
+    
+    // file
+    out << "  ";
+    for (int i='a'; i <= 'h'; i++) {
+        out << i;
+    }
+    
+    out << std::endl;
+}
--- src/render/renderostream.h
+++ src/render/renderostream.h
@@ -1,9 +1,19 @@
 #ifndef RENDER_OSTREAM_H_
 #define RENDER_OSTREAM_H_
 #include "chessrender.h"
+#include <iostream>
+
+// forward declaration for Board ptr
+class Board;
 
 class RenderOStream : public ChessRender{
-
+private:
+    std::ostream& out;
+public:
+    // you may only construct a RenderOStream given a
+    // board to reference and ostream to print to
+    RenderOStream(Board* board, std::ostream&);
+    void render() const override;
 };
 
 #endif
