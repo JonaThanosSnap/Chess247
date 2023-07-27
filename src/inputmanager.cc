ad38ec89ac8f9f17d7b10bb97b567418ce068c8e
Matthews Ma <matthewsma21@gmail.com>
2023-07-27 01:08:05 -0400

    Start InputManager class
    Todo:
    - get specifics params for each class
    - call correct functions

--- src/inputmanager.cc
+++ src/inputmanager.cc
@@ -1,1 +1,27 @@
 #include "inputmanager.h"
+#include <string>
+#include <iostream>
+
+void InputManager::handleInput() {
+    // read in one command at a time and handle it
+    std::string cmd;
+    std::cin >> cmd;
+
+    if (cmd == "game") {
+        std::string white, black;
+        std::cin >> white >> black;
+
+    } else if (cmd == "resign") {
+        // concede game to opponent
+
+    } else if (cmd == "move") {
+        std::string start, end;
+        std::cin >> start >> end;
+
+    } else if (cmd == "setup") {
+        
+
+    } else {
+        std::cout << "Invalid command: " << cmd << std::endl;
+    }
+}
--- src/inputmanager.h
+++ src/inputmanager.h
@@ -1,8 +1,14 @@
 #ifndef INPUT_MANAGER_H_
 #define INPUT_MANAGER_H_
 
-class InputManager {
+class Chess;
 
+// translates inputs into commands for chess game
+class InputManager {
+private:
+    Chess* const game;
+public:
+    void handleInput();
 };
 
 #endif
